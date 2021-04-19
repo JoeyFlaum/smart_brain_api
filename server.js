c95e615fe74938f8ec9c34a858d081905e1e0301
Therealaveragejoe <58762590+Therealaveragejoe@users.noreply.github.com>
2021-04-18 23:06:42 -0500

    change image route to put

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.get('/profile/:id',(req,res)=>{
 })
 
 // /image --> PUT --> user
-app.post('/image', (req, res) =>{
+app.put('/image', (req, res) =>{
   const {id} = req.body;
   let found = false;
   database.users.forEach(user =>{
@@ -88,6 +88,7 @@ app.post('/image', (req, res) =>{
   if (!found){
     res.status(400).json("not found")
   }
+  console.log(database.users[0].entries)
 })
 
 app.listen(3000, () => {
