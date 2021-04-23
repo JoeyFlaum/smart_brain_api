0b272d02550267a525e7ade553c6031f3304981b
Therealaveragejoe <58762590+Therealaveragejoe@users.noreply.github.com>
2021-04-22 23:07:51 -0500

    update endpoints to work with database

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 const express = require("express");
 const app = express();
-const bcrypt = require('bcrypt-nodejs')
-const cors = require('cors')
-const knex = require('knex')
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
 
 const db = knex({
-    client: 'pg',
-    connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'GooseNugg3t@B(',
-      database : 'smart-brain'
-    }
-  });
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "GooseNugg3t@B(",
+    database: "smart-brain",
+  },
+});
 
-db.select('*').from('users').then(data =>{
-  console.log(data)
-})
+// db.select('*').from('users').then(data =>{
+//   console.log(data)
+// })
 
-app.use(cors())
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -62,49 +62,43 @@ app.post("/signin", (req, res) => {
 // /register --> POST = user
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
-  db('users')
-  .returning("*")
-  .insert({
-    email: email,
-    name:name,
-    joined: new Date()
-  }).then(user=>res.json(user[0]))
-  .catch(err =>res.status(400).json("unable to register"))
+  db("users")
+    .returning("*")
+    .insert({
+      email: email,
+      name: name,
+      joined: new Date(),
+    })
+    .then((user) => res.json(user[0]))
+    .catch((err) => res.status(400).json("unable to register"));
 });
 
 // /register --> POST = user
-app.get('/profile/:id',(req,res)=>{
-  const {id} = req.params;
-  let found = false;
-  database.users.forEach(user =>{
-    if (user.id === id){
-      found = true;
-     return res.json(user);
-    }
-  })
-    if (!found){
-      res.status(400).json("not found")
-    }
-})
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  db.select("*")
+    .from("users")
+    .where({ id: id })
+    .then((user) => {
+      if (user.length) {
+        return res.json(user[0]);
+      } else {
+        res.status(400).json("not found");
+      }
+    })
+    .catch((err) => res.status(400).json("error getting user"));
+});
 
 // /image --> PUT --> user
-app.put('/image', (req, res) =>{
-  const {id} = req.body;
-  let found = false;
-  database.users.forEach(user =>{
-    if (user.id === id){
-      found = true;
-      user.entries++
-     return res.json(user.entries);
-    }
-  })
-  if (!found){
-    res.status(400).json("not found")
-  }
-  console.log(database.users[0].entries)
-})
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  db('users').where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {res.json(entries[0])})
+    .catch(err => res.status(400).json('unable to get entries'));
+});
 
 app.listen(3000, () => {
   console.log("app is running on port 3000");
 });
-
